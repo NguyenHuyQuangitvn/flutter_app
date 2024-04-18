a59ab82233762cb9de3b5000f55c439a8f868524
Quang-BugCreater <nguyenhuyquang.2001@gmail.com>
2024-04-18 10:42:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ulearning
+
+dart run build_runner build
+
+2023 12 18
+flutter update version 3.1.5
+# flutter_app
---
+++ lib/common/values/constant.dart
@@ -0,0 +1,19 @@
+// baidu yapi
+//const SERVER_API_URL = 'https://ulearning.codemain.top/';
+//  const SERVER_API_URL = 'http://127.0.0.1:8000/';
+const SERVER_API_URL = 'http://10.0.2.2:8000/';
+/// 用户 - 配置信息
+const String STORAGE_USER_PROFILE_KEY = 'user_profile';
+
+/// 用户 - 配置信息
+const String STORAGE_USER_TOKEN_KEY = 'user_token';
+
+/// 设备是否第一次打开
+const String STORAGE_DEVICE_FIRST_OPEN_KEY = 'device_first_open';
+
+/// 多语言
+const String STORAGE_LANGUAGE_CODE = 'language_code';
+
+const String APPID = "2b8731acd0d4481e851913553c702663";
+
+
